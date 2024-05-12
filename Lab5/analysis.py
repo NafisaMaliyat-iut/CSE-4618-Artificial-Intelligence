13c61e73dc49dfc01360f2c4bb4df546fffcd877
NafisaMaliyat-iut <nafisamaliyat@iut-dhaka.edu>
2024-05-12 10:59:31 +0600

    Lab 5 progress

---
+++ Lab5/analysis.py
@@ -0,0 +1,78 @@
+# analysis.py
+# -----------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+######################
+# ANALYSIS QUESTIONS #
+######################
+
+# Set the given parameters to obtain the specified policies through
+# value iteration.
+
+def question2():
+    answerDiscount = 0.9
+    answerNoise = 0 
+    return answerDiscount, answerNoise
+
+def question3a(): 
+    # i kept the discount low (takes risks) with no noise (ends ups where intended--> so does not fall into red tiles) and no living reward (take short way-->dont try to live longer)
+    answerDiscount = 0.2 
+    answerNoise = 0
+    answerLivingReward = 0 
+    return answerDiscount, answerNoise, answerLivingReward
+    # If not possible, return 'NOT POSSIBLE'
+
+def question3b():
+    # introduced living reward so it takes the long way 
+    answerDiscount = 0.2
+    answerNoise = 0.1
+    answerLivingReward = 0.5
+    return answerDiscount, answerNoise, answerLivingReward
+    # If not possible, return 'NOT POSSIBLE'
+
+def question3c():
+    # higher discount to go to +10 but not so high that it does not take risks 
+    answerDiscount = 0.7
+    answerNoise = 0.1
+    answerLivingReward = 0
+    return answerDiscount, answerNoise, answerLivingReward
+    # If not possible, return 'NOT POSSIBLE'
+
+def question3d():
+    # introduced living reward to take long road ahead 
+    answerDiscount = 0.7
+    answerNoise = 0.1
+    answerLivingReward = 0.8
+    return answerDiscount, answerNoise, answerLivingReward
+    # If not possible, return 'NOT POSSIBLE'
+
+def question3e():
+    # make the living reward super high so it does not want to terminate  
+    answerDiscount = 0
+    answerNoise = 0.2
+    answerLivingReward = 0.9
+    return answerDiscount, answerNoise, answerLivingReward
+    # If not possible, return 'NOT POSSIBLE'
+
+def question8():
+    answerEpsilon = None
+    answerLearningRate = None
+    return answerEpsilon, answerLearningRate
+    # If not possible, return 'NOT POSSIBLE'
+
+if __name__ == '__main__':
+    print('Answers to analysis questions:')
+    import analysis
+    for q in [q for q in dir(analysis) if q.startswith('question')]:
+        response = getattr(analysis, q)()
+        print('  Question %s:\t%s' % (q, str(response)))
